42d6d10f3b75173a2456e1af5cd75ee0d61df1e3
Bankaimaster999 <bankaimaster999@gmail.com>
2020-11-26 13:13:04 -0400

    Adjustment of Load-out pop up position

--- Source/Core/VideoCommon/OnScreenDisplay.cpp
+++ Source/Core/VideoCommon/OnScreenDisplay.cpp
@@ -21,8 +21,8 @@
 
 namespace OSD
 {
-constexpr float LEFT_MARGIN = 10.0f;    // Pixels to the left of OSD messages.
-constexpr float TOP_MARGIN = 10.0f;     // Pixels above the first OSD message.
+constexpr float LEFT_MARGIN = 270.0f;    // Pixels to the left of OSD messages.
+constexpr float TOP_MARGIN = 5.0f;     // Pixels above the first OSD message.
 constexpr float WINDOW_PADDING = 4.0f;  // Pixels between subsequent OSD messages.
 
 static std::atomic<int> s_obscured_pixels_left = 0;
